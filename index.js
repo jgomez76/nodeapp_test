1db098ffdd316890ca3ad1bd1876857364488a6f
jgomez76 <54595754+jgomez76@users.noreply.github.com>
2022-06-16 17:15:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/will', function (req, res) {
     res.send('{ "response": "Hello World" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " FUNCIONA!!! " }');
+    res.send('{ "response": " AHORA FUNCIONA!!! " }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
