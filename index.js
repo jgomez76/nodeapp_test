c41c2eeac7cbf124cb96c6f4b55c2ce2b5dc002a
jgomez76 <54595754+jgomez76@users.noreply.github.com>
2022-06-17 09:15:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Prueba de commit desde webhook" }');
+    res.send('{ "response": "Otra prueba" }');
 });
 
 app.get('/will', function (req, res) {
